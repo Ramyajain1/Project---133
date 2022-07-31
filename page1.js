f52c7057c2504e7d8f24ef0874eb4dc1a2ccc692
Ramya jain <91374142+Ramyajain1@users.noreply.github.com>
2022-07-31 15:09:56 +0530

    Add files via upload

---
+++ page1.js
@@ -0,0 +1,46 @@
+img = "";
+objects = [];
+status = "";
+
+function preload() {
+    img = loadImage('page1_img.jpg');
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+
+    if (status != "") {
+
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            fill("#56BAED");
+            percent = floor(objects[0].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#56BAED");
+            rect(objects[0].x, objects[0].y, objects[0].width, objects[0].height);
+        }
+    }
+}
